d55fe89cb1d9e79fb7d5893147781a5dcd8c0902
scratch-cmd <63279385+scratch-cmd@users.noreply.github.com>
2020-12-11 20:51:39 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+led.plot_brightness(4, 4, 20)
+
 def on_forever():
-    pass
+    if True:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    else:
+        led.unplot(0, 4)
+        led.unplot(0, 3)
+        led.unplot(0, 2)
+        led.unplot(0, 1)
+        led.unplot(0, 0)
 basic.forever(on_forever)
