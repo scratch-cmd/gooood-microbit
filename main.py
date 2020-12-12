c99b1b215372a388791b6cfd8acb4d06aa89632d
scratch-cmd <63279385+scratch-cmd@users.noreply.github.com>
2020-12-12 13:26:43 +0800

    更新 main.blocks, main.py, test.ts

--- main.py
+++ main.py
@@ -1,18 +1,164 @@
-led.plot_brightness(4, 4, 20)
+def on_button_pressed_ab():
+    input.calibrate_compass()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_forever():
-    if True:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
+    led.plot_brightness(4, 4, 20)
+    led.plot_brightness(4, 3, 20)
+basic.forever(on_forever)
+
+def on_forever2():
+    if input.magnetic_force(Dimension.STRENGTH) > 0 and input.magnetic_force(Dimension.STRENGTH) <= 100:
+        led.plot(0, 4)
+        led.unplot(0, 1)
+        led.unplot(0, 3)
+        led.unplot(0, 2)
+        led.unplot(0, 0)
+    elif input.magnetic_force(Dimension.STRENGTH) > 100 and input.magnetic_force(Dimension.STRENGTH) <= 200:
+        led.plot(0, 3)
+        led.plot(0, 4)
+        led.unplot(0, 1)
+        led.unplot(0, 2)
+        led.unplot(0, 0)
+    elif input.magnetic_force(Dimension.STRENGTH) > 200 and input.magnetic_force(Dimension.STRENGTH) <= 300:
+        led.plot(0, 2)
+        led.plot(0, 3)
+        led.plot(0, 4)
+        led.unplot(0, 0)
+        led.unplot(0, 1)
+    elif input.magnetic_force(Dimension.STRENGTH) > 300 and input.magnetic_force(Dimension.STRENGTH) <= 400:
+        led.plot(0, 1)
+        led.plot(0, 2)
+        led.plot(0, 3)
+        led.plot(0, 4)
+        led.unplot(0, 0)
+    elif input.magnetic_force(Dimension.STRENGTH) > 400:
+        led.plot(0, 0)
+        led.plot(0, 4)
+        led.plot(0, 3)
+        led.plot(0, 2)
+        led.plot(0, 1)
     else:
         led.unplot(0, 4)
         led.unplot(0, 3)
         led.unplot(0, 2)
         led.unplot(0, 1)
         led.unplot(0, 0)
-basic.forever(on_forever)
+basic.forever(on_forever2)
+
+def on_forever3():
+    if input.magnetic_force(Dimension.Y) > 0 and input.magnetic_force(Dimension.Y) <= 100:
+        led.plot(2, 4)
+        led.unplot(2, 1)
+        led.unplot(2, 3)
+        led.unplot(2, 2)
+        led.unplot(2, 0)
+    elif input.magnetic_force(Dimension.Y) > 100 and input.magnetic_force(Dimension.Y) <= 200:
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.unplot(0, 1)
+        led.unplot(2, 2)
+        led.unplot(2, 0)
+    elif input.magnetic_force(Dimension.Y) > 200 and input.magnetic_force(Dimension.Y) <= 300:
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.unplot(2, 0)
+        led.unplot(2, 1)
+    elif input.magnetic_force(Dimension.Y) > 300 and input.magnetic_force(Dimension.Y) <= 400:
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.unplot(2, 0)
+    elif input.magnetic_force(Dimension.Y) > 400:
+        led.plot(2, 0)
+        led.plot(2, 4)
+        led.plot(2, 3)
+        led.plot(2, 2)
+        led.plot(2, 1)
+    else:
+        led.unplot(2, 4)
+        led.unplot(2, 3)
+        led.unplot(2, 2)
+        led.unplot(2, 1)
+        led.unplot(2, 0)
+basic.forever(on_forever3)
+
+def on_forever4():
+    if input.magnetic_force(Dimension.X) > 0 and input.magnetic_force(Dimension.X) <= 100:
+        led.plot(1, 4)
+        led.unplot(1, 1)
+        led.unplot(1, 3)
+        led.unplot(1, 2)
+        led.unplot(1, 0)
+    elif input.magnetic_force(Dimension.X) > 100 and input.magnetic_force(Dimension.X) <= 200:
+        led.plot(1, 3)
+        led.plot(1, 4)
+        led.unplot(1, 1)
+        led.unplot(1, 2)
+        led.unplot(1, 0)
+    elif input.magnetic_force(Dimension.X) > 200 and input.magnetic_force(Dimension.X) <= 300:
+        led.plot(1, 2)
+        led.plot(1, 3)
+        led.plot(1, 4)
+        led.unplot(1, 0)
+        led.unplot(1, 1)
+    elif input.magnetic_force(Dimension.X) > 300 and input.magnetic_force(Dimension.X) <= 400:
+        led.plot(1, 1)
+        led.plot(1, 2)
+        led.plot(1, 3)
+        led.plot(1, 4)
+        led.unplot(1, 0)
+    elif input.magnetic_force(Dimension.X) > 400:
+        led.plot(1, 0)
+        led.plot(1, 4)
+        led.plot(1, 3)
+        led.plot(1, 2)
+        led.plot(1, 1)
+    else:
+        led.unplot(1, 4)
+        led.unplot(1, 3)
+        led.unplot(1, 2)
+        led.unplot(1, 1)
+        led.unplot(1, 0)
+basic.forever(on_forever4)
+
+def on_forever5():
+    if input.magnetic_force(Dimension.Z) > 0 and input.magnetic_force(Dimension.Z) <= 100:
+        led.plot(3, 4)
+        led.unplot(3, 1)
+        led.unplot(3, 3)
+        led.unplot(3, 2)
+        led.unplot(3, 0)
+    elif input.magnetic_force(Dimension.Z) > 100 and input.magnetic_force(Dimension.Z) <= 200:
+        led.plot(3, 3)
+        led.plot(3, 4)
+        led.unplot(3, 1)
+        led.unplot(3, 2)
+        led.unplot(3, 0)
+    elif input.magnetic_force(Dimension.Z) > 200 and input.magnetic_force(Dimension.Z) <= 300:
+        led.plot(3, 2)
+        led.plot(3, 3)
+        led.plot(3, 4)
+        led.unplot(3, 0)
+        led.unplot(3, 1)
+    elif input.magnetic_force(Dimension.Z) > 300 and input.magnetic_force(Dimension.Z) <= 400:
+        led.plot(3, 1)
+        led.plot(3, 2)
+        led.plot(3, 3)
+        led.plot(3, 4)
+        led.unplot(3, 0)
+    elif input.magnetic_force(Dimension.Z) > 400:
+        led.plot(3, 0)
+        led.plot(3, 4)
+        led.plot(3, 3)
+        led.plot(3, 2)
+        led.plot(3, 1)
+    else:
+        led.unplot(3, 4)
+        led.unplot(3, 3)
+        led.unplot(3, 2)
+        led.unplot(3, 1)
+        led.unplot(3, 0)
+basic.forever(on_forever5)
--- test.ts
+++ test.ts
@@ -1,1 +1,163 @@
-// 在此处测试；当此软件包作为插件使用时，将不会编译此软件包。
+input.onButtonPressed(Button.AB, function () {
+    input.calibrateCompass()
+})
+basic.forever(function () {
+    led.plotBrightness(4, 4, 20)
+    led.plotBrightness(4, 3, 20)
+})
+basic.forever(function () {
+    if (input.magneticForce(Dimension.Strength) > 0 && input.magneticForce(Dimension.Strength) <= 100) {
+        led.plot(0, 4)
+        led.unplot(0, 1)
+        led.unplot(0, 3)
+        led.unplot(0, 2)
+        led.unplot(0, 0)
+    } else if (input.magneticForce(Dimension.Strength) > 100 && input.magneticForce(Dimension.Strength) <= 200) {
+        led.plot(0, 3)
+        led.plot(0, 4)
+        led.unplot(0, 1)
+        led.unplot(0, 2)
+        led.unplot(0, 0)
+    } else if (input.magneticForce(Dimension.Strength) > 200 && input.magneticForce(Dimension.Strength) <= 300) {
+        led.plot(0, 2)
+        led.plot(0, 3)
+        led.plot(0, 4)
+        led.unplot(0, 0)
+        led.unplot(0, 1)
+    } else if (input.magneticForce(Dimension.Strength) > 300 && input.magneticForce(Dimension.Strength) <= 400) {
+        led.plot(0, 1)
+        led.plot(0, 2)
+        led.plot(0, 3)
+        led.plot(0, 4)
+        led.unplot(0, 0)
+    } else if (input.magneticForce(Dimension.Strength) > 400) {
+        led.plot(0, 0)
+        led.plot(0, 4)
+        led.plot(0, 3)
+        led.plot(0, 2)
+        led.plot(0, 1)
+    } else {
+        led.unplot(0, 4)
+        led.unplot(0, 3)
+        led.unplot(0, 2)
+        led.unplot(0, 1)
+        led.unplot(0, 0)
+    }
+})
+basic.forever(function () {
+    if (input.magneticForce(Dimension.Y) > 0 && input.magneticForce(Dimension.Y) <= 100) {
+        led.plot(2, 4)
+        led.unplot(2, 1)
+        led.unplot(2, 3)
+        led.unplot(2, 2)
+        led.unplot(2, 0)
+    } else if (input.magneticForce(Dimension.Y) > 100 && input.magneticForce(Dimension.Y) <= 200) {
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.unplot(0, 1)
+        led.unplot(2, 2)
+        led.unplot(2, 0)
+    } else if (input.magneticForce(Dimension.Y) > 200 && input.magneticForce(Dimension.Y) <= 300) {
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.unplot(2, 0)
+        led.unplot(2, 1)
+    } else if (input.magneticForce(Dimension.Y) > 300 && input.magneticForce(Dimension.Y) <= 400) {
+        led.plot(2, 1)
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.unplot(2, 0)
+    } else if (input.magneticForce(Dimension.Y) > 400) {
+        led.plot(2, 0)
+        led.plot(2, 4)
+        led.plot(2, 3)
+        led.plot(2, 2)
+        led.plot(2, 1)
+    } else {
+        led.unplot(2, 4)
+        led.unplot(2, 3)
+        led.unplot(2, 2)
+        led.unplot(2, 1)
+        led.unplot(2, 0)
+    }
+})
+basic.forever(function () {
+    if (input.magneticForce(Dimension.X) > 0 && input.magneticForce(Dimension.X) <= 100) {
+        led.plot(1, 4)
+        led.unplot(1, 1)
+        led.unplot(1, 3)
+        led.unplot(1, 2)
+        led.unplot(1, 0)
+    } else if (input.magneticForce(Dimension.X) > 100 && input.magneticForce(Dimension.X) <= 200) {
+        led.plot(1, 3)
+        led.plot(1, 4)
+        led.unplot(1, 1)
+        led.unplot(1, 2)
+        led.unplot(1, 0)
+    } else if (input.magneticForce(Dimension.X) > 200 && input.magneticForce(Dimension.X) <= 300) {
+        led.plot(1, 2)
+        led.plot(1, 3)
+        led.plot(1, 4)
+        led.unplot(1, 0)
+        led.unplot(1, 1)
+    } else if (input.magneticForce(Dimension.X) > 300 && input.magneticForce(Dimension.X) <= 400) {
+        led.plot(1, 1)
+        led.plot(1, 2)
+        led.plot(1, 3)
+        led.plot(1, 4)
+        led.unplot(1, 0)
+    } else if (input.magneticForce(Dimension.X) > 400) {
+        led.plot(1, 0)
+        led.plot(1, 4)
+        led.plot(1, 3)
+        led.plot(1, 2)
+        led.plot(1, 1)
+    } else {
+        led.unplot(1, 4)
+        led.unplot(1, 3)
+        led.unplot(1, 2)
+        led.unplot(1, 1)
+        led.unplot(1, 0)
+    }
+})
+basic.forever(function () {
+    if (input.magneticForce(Dimension.Z) > 0 && input.magneticForce(Dimension.Z) <= 100) {
+        led.plot(3, 4)
+        led.unplot(3, 1)
+        led.unplot(3, 3)
+        led.unplot(3, 2)
+        led.unplot(3, 0)
+    } else if (input.magneticForce(Dimension.Z) > 100 && input.magneticForce(Dimension.Z) <= 200) {
+        led.plot(3, 3)
+        led.plot(3, 4)
+        led.unplot(3, 1)
+        led.unplot(3, 2)
+        led.unplot(3, 0)
+    } else if (input.magneticForce(Dimension.Z) > 200 && input.magneticForce(Dimension.Z) <= 300) {
+        led.plot(3, 2)
+        led.plot(3, 3)
+        led.plot(3, 4)
+        led.unplot(3, 0)
+        led.unplot(3, 1)
+    } else if (input.magneticForce(Dimension.Z) > 300 && input.magneticForce(Dimension.Z) <= 400) {
+        led.plot(3, 1)
+        led.plot(3, 2)
+        led.plot(3, 3)
+        led.plot(3, 4)
+        led.unplot(3, 0)
+    } else if (input.magneticForce(Dimension.Z) > 400) {
+        led.plot(3, 0)
+        led.plot(3, 4)
+        led.plot(3, 3)
+        led.plot(3, 2)
+        led.plot(3, 1)
+    } else {
+        led.unplot(3, 4)
+        led.unplot(3, 3)
+        led.unplot(3, 2)
+        led.unplot(3, 1)
+        led.unplot(3, 0)
+    }
+})
